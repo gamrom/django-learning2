91c5a5798478d97422a7b04394d44d10f359e4d6
gamrom <gamrom01@naver.com>
2020-04-17 15:17:31 +0900

    form과 admin까지 공부

---
+++ mighty_django/posts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+# Create your views here.
+
+def new(request):
+    form = PostForm()
+    return render(request, 'new.html', {'form': form})
+
+def create(request):
+    if request.method == "POST":
+        # title = request.POST.get('title')
+        # content = request.POST.get('content')
+        # Post.objects.create(title= title, content= content)
+        # Post.save()
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('main_index')
